403e14a1f3f718e1af6628992e9a46ae3304a8ae
ArafinNabil <nabil0111972@gmail.com>
2026-01-10 21:34:25 +0600

    todo done

--- 2-main-exercise-file/scripts/11-array-todolist.js
+++ 2-main-exercise-file/scripts/11-array-todolist.js
@@ -1,4 +1,12 @@
-  const todoList = ['watch tv', 'push code'];
+  const todoList = [
+    {
+      name:'watch tv',
+      dueDate:'2028-12-05'
+    },{ 
+      name:'push code',
+      dueDate:'2028-12-05'
+    }
+  ];
 
   renderTodoList()
 
@@ -7,19 +15,37 @@
     let todoHtml = '';
 
     for (let i = 0; i < todoList.length; i++) {
-      const todo = todoList[i];
-      html = `<p>${todo}</p>`;
+      const todoObject = todoList[i];
+      const {name, dueDate} = todoObject;
+      
+      html = 
+      `<p>
+       ${name} ${dueDate}
+        <button onclick="todoList.splice(${i},1);
+        renderTodoList();">delete
+        </button> 
+      </p>`;
       todoHtml += html ;
     
       document.querySelector('.js-display-todo').innerHTML = todoHtml;  
     };
+    console.log(todoHtml)
   };
 
   function addTodo() {
+    
     let inputElem = document.querySelector('.js-input');
     let name = inputElem.value;
 
-    todoList.push(name);
+    let dateInputElem = document.querySelector('.js-date-input');
+    let dueDate = dateInputElem.value;
+
+    todoList.push({
+      name:name, 
+      dueDate:dueDate
+      // name,
+      // dueDate
+    });
 
     inputElem.value = '';
     renderTodoList();
